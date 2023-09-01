ca2ce49cf7464deb3b6ebf446d1b063a2996aa1b
Typist <typist@example.com>
2023-09-01 15:32:09 +0000

    tests with mock and spy

---
+++ c_cmocka/test-cmocka/hvac_test_with_mock/hvac_test.c
@@ -0,0 +1,77 @@
+#include <stdarg.h>
+#include <setjmp.h>
+#include <stddef.h>
+#include "cmocka.h"
+
+#include "HVAC.h"
+
+#include "ThermostatController.h"
+
+
+void __wrap_HVAC_set_cooler(bool active)
+{
+    function_called();
+    check_expected(active);
+}
+
+void __wrap_HVAC_set_heater(bool active)
+{
+    function_called();
+    check_expected(active);
+}
+
+float __wrap_HVAC_get_desired_temperature()
+{
+    return mock_type(float);
+}
+
+float __wrap_HVAC_get_current_temperature()
+{
+    return mock_type(float);
+}
+
+static void test_regulate_temperature_too_cold(void **state)
+{
+    expect_function_call(__wrap_HVAC_set_heater);
+    expect_function_call(__wrap_HVAC_set_cooler);
+    will_return(__wrap_HVAC_get_desired_temperature, 23);
+    will_return(__wrap_HVAC_get_current_temperature, 20);
+    expect_value(__wrap_HVAC_set_heater, active, true);
+    expect_value(__wrap_HVAC_set_cooler, active, false);
+
+    regulate_temperature();
+}
+static void test_regulate_temperature_too_hot(void **state)
+{
+    expect_function_call(__wrap_HVAC_set_heater);
+    expect_function_call(__wrap_HVAC_set_cooler);
+    will_return(__wrap_HVAC_get_desired_temperature, 23);
+    will_return(__wrap_HVAC_get_current_temperature, 27);
+    expect_value(__wrap_HVAC_set_heater, active, false);
+    expect_value(__wrap_HVAC_set_cooler, active, true);
+
+    regulate_temperature();
+}
+
+static void test_regulate_temperature_ok(void **state)
+{
+    expect_function_call(__wrap_HVAC_set_heater);
+    expect_function_call(__wrap_HVAC_set_cooler);
+    will_return(__wrap_HVAC_get_desired_temperature, 23);
+    will_return(__wrap_HVAC_get_current_temperature, 23);
+    expect_value(__wrap_HVAC_set_heater, active, false);
+    expect_value(__wrap_HVAC_set_cooler, active, false);
+
+    regulate_temperature();
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] =
+            {
+            cmocka_unit_test(test_regulate_temperature_too_cold),
+            cmocka_unit_test(test_regulate_temperature_too_hot),
+            cmocka_unit_test(test_regulate_temperature_ok),
+            };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
---
+++ c_cmocka/test-cmocka/hvac_test_with_spy/hvac_test.c
@@ -0,0 +1,83 @@
+#include <stdarg.h>
+#include <setjmp.h>
+#include <stddef.h>
+#include "cmocka.h"
+
+#include "HVAC.h"
+
+#include "ThermostatController.h"
+
+bool cooler_active;
+bool heater_active;
+
+void __wrap_HVAC_set_cooler(bool active)
+{
+    cooler_active = active;
+}
+
+void __wrap_HVAC_set_heater(bool active)
+{
+    heater_active = active;
+}
+
+float __wrap_HVAC_get_desired_temperature()
+{
+    return mock_type(float);
+}
+
+float __wrap_HVAC_get_current_temperature()
+{
+    return mock_type(float);
+}
+
+static void test_regulate_temperature_too_cold(void **state)
+{
+    cooler_active = false;
+    heater_active = false;
+
+    will_return(__wrap_HVAC_get_desired_temperature, 23);
+    will_return(__wrap_HVAC_get_current_temperature, 20);
+
+    regulate_temperature();
+
+    assert_int_equal(true, heater_active);
+    assert_int_equal(false, cooler_active);
+}
+static void test_regulate_temperature_too_hot(void **state)
+{
+    cooler_active = false;
+    heater_active = false;
+
+    will_return(__wrap_HVAC_get_desired_temperature, 23);
+    will_return(__wrap_HVAC_get_current_temperature, 27);
+
+    regulate_temperature();
+
+    assert_int_equal(false, heater_active);
+    assert_int_equal(true, cooler_active);
+}
+
+static void test_regulate_temperature_ok(void **state)
+{
+    cooler_active = false;
+    heater_active = false;
+
+    will_return(__wrap_HVAC_get_desired_temperature, 23);
+    will_return(__wrap_HVAC_get_current_temperature, 23);
+
+    regulate_temperature();
+
+    assert_int_equal(false, heater_active);
+    assert_int_equal(false, cooler_active);
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] =
+            {
+            cmocka_unit_test(test_regulate_temperature_too_cold),
+            cmocka_unit_test(test_regulate_temperature_too_hot),
+            cmocka_unit_test(test_regulate_temperature_ok),
+            };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
